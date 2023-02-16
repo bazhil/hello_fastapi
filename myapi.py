ac2bb09881d9580bc5b181dcd29d0c3071c56f6a
bazhil <i-bazh@mail.ru>
2023-02-16 20:19:17 +0300

    Use parameters

--- myapi.py
+++ myapi.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
+
+students = {
+    1: {
+        'name': 'John',
+        'age': 17,
+        'class': 'Year 12'
+    }
+}
 
 @app.get('/')
 def index():
     return {'name': 'First Data'}
+
+@app.get('/get-student/{student_id}')
+def get_student(student_id: int = Path(None, description=f'ID of the student you want to view.', gt=0)):
+    return students[student_id]
