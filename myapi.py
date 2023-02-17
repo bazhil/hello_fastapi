74221ed86a0e0f329cb89692cfaf3fe34d29e25f
bazhil <i-bazh@mail.ru>
2023-02-18 01:43:51 +0300

    Use Path and Query parameters

--- myapi.py
+++ myapi.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Path
+from typing import Optional
 
 app = FastAPI()
 
@@ -16,4 +17,22 @@ def index():
 
 @app.get('/get-student/{student_id}')
 def get_student(student_id: int = Path(None, description=f'ID of the student you want to view.', gt=0)):
+    """Path parameter"""
     return students[student_id]
+
+
+# @app.get('/get-by-name/')
+# def get_student(name: Optional[str] = None):
+#     """Query parameter"""
+#     for student_id in students:
+#         if students[student_id]['name'] == name:
+#             return students[student_id]
+#     return {'Data': 'Not Found'}
+
+@app.get('/get-by-name/{student_id}')
+def get_student(*, student_id: int, name: Optional[str] = None, test: int):
+    """Path parameters and query parameters"""
+    for student_id in students:
+        if students[student_id]['name'] == name:
+            return students[student_id]
+    return {'Data': 'Not Found'}
