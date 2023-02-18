c02ad68022346f24845f973628bde81f979a89e3
bazhil <i-bazh@mail.ru>
2023-02-18 21:09:23 +0300

    finish

--- myapi.py
+++ myapi.py
@@ -18,6 +18,11 @@ class Student(BaseModel):
     year: str
 
 
+class UpdateStudent(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    year: Optional[str] = None
+
 @app.get('/')
 def index():
     return {'name': 'First Data'}
@@ -52,3 +57,25 @@ def create_student(student_id: int, student: Student):
 
     return students[student_id]
 
+@app.put('/update-student/{student_id}')
+def update_student(student_id: int, student: Student):
+    if student_id not in students:
+        return {'Error': 'Student does not exist'}
+
+    if student.name != None:
+        students[student_id].name = student.name
+    if student.age != None:
+        students[student_id].age = student.age
+    if student.year != None:
+        students[student_id].year = student.year
+
+    return students[student_id]
+
+@app.delete('/delete-student/{student_id}')
+def delete_student(student_id: int):
+    if student_id not in students:
+        return {'Error': 'Student does not exist'}
+
+    del students[student_id]
+
+    return {'Message': 'Student deleted successfully.'}
